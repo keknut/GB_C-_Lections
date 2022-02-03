f23588722f79f30a15e62a52a83e33d7a77eaa19
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-03 18:02:40 +0400

    Replace all files from Lection 1 in folder Lection_1

---
+++ Lection_1/Example002_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введи user name: ");
+string userName = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(userName);
---
+++ Lection_1/Example005_ifElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+
+if(userName == "keknut"){
+    Console.Write("Дароу, ");
+    Console.Write(userName);
+}else{
+    Console.Write("Привет, ");
+    Console.Write(userName);
+}
