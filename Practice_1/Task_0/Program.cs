9fdce021a6720671150fa4c47dab849331887168
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-04 16:28:49 +0400

    Modified Task_0

--- Practice_1/Task_0/Program.cs
+++ Practice_1/Task_0/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вывести квадрат числа
+
+int number = 9;
+Console.Write("Квадрат вашего числа: " + number*number);
