a8efa91f6edabb09b14d2fade488f02e590391de
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-06 21:53:21 +0400

    Add commentary in task_1

--- Practice_1/Task_1/Program.cs
+++ Practice_1/Task_1/Program.cs
@@ -1,11 +1,11 @@
 ﻿// По двум заданным числам проверять является ли первое квадратом второго
 
-Console.Write("Input number 1: ");
-int number_1 = int.Parse(Console.ReadLine());
-Console.Write("Input number 2: ");
-int number_2 = int.Parse(Console.ReadLine());
-if (number_1 == number_2 * number_2){
+Console.Write("Input number 1: "); //выводит на экран "введите первое число"
+int number_1 = int.Parse(Console.ReadLine()); //Считывает в консоли число и присваивает его переменной number_1
+Console.Write("Input number 2: "); //выводит на экран "введите второе число"
+int number_2 = int.Parse(Console.ReadLine()); //Считывает в консоли число и присваивает его переменной number_2
+if (number_1 == number_2 * number_2){ //если квадрат второго числа, равен первому, то выводим "да"
     Console.WriteLine("Yes!");
-}else{
+}else{ //иначе выводим "нет"
     Console.WriteLine("No(");
 }
