735d2920b8e2c68999d1ce4b1d19e5285e0c5cc7
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-06 22:00:10 +0400

    Completing the Task_3

--- Practice_1/Task_3/Program.cs
+++ Practice_1/Task_3/Program.cs
@@ -1,1 +1,25 @@
 ﻿// По заданному номеру дня недели вывести его название
+
+Console.Write("Input day of the week number: ");
+int dayNumber = int.Parse(Console.ReadLine());
+if(dayNumber == 1){
+    Console.Write("Monday");
+}
+if(dayNumber == 2){
+    Console.Write("Tuesday");
+}
+if(dayNumber == 3){
+    Console.Write("Wednesday");
+}
+if(dayNumber == 4){
+    Console.Write("Thursday");
+}
+if(dayNumber == 5){
+    Console.Write("Friday");
+}
+if(dayNumber == 6){
+    Console.Write("Saturday");
+}
+if(dayNumber == 7){
+    Console.Write("Sunday");
+}
