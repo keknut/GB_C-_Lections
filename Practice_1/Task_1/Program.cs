77b6211c52b6328058f24bb4722c953e48315861
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-06 21:42:09 +0400

    completed the task_1

--- Practice_1/Task_1/Program.cs
+++ Practice_1/Task_1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Input number 1: ");
+int number_1 = int.Parse(Console.ReadLine());
+Console.Write("Input number 2: ");
+int number_2 = int.Parse(Console.ReadLine());
+if (number_1 == number_2 * number_2){
+    Console.WriteLine("Yes!");
+}else{
+    Console.WriteLine("No(");
+}
