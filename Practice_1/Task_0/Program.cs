3fb58b2516f6548a3823d253f439654605189666
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-06 21:28:05 +0400

    Add in task_0 input variation

--- Practice_1/Task_0/Program.cs
+++ Practice_1/Task_0/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Вывести квадрат числа
 
-int number = new Random().Next(1, 10);
-Console.WriteLine("n=" + number);
+//int number = new Random().Next(1, 10); //присваивает переменной случайное число 1.. 10
+Console.Write("Input number:");
+int number = int.Parse(Console.ReadLine());
 Console.Write("Квадрат вашего числа: " + number*number);
