69072298d1ae078d51e03992ac529f96d904a616
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-06 22:52:57 +0400

    Completing the Task_2

--- Practice_1/Task_2/Program.cs
+++ Practice_1/Task_2/Program.cs
@@ -1,1 +1,17 @@
 ﻿// Даны два числа. Показать большее и меньшее число
+
+Console.Write("Input first number: ");
+int number_1 = int.Parse(Console.ReadLine());
+Console.Write("Input second number: ");
+int number_2 = int.Parse(Console.ReadLine());
+if(number_1 > number_2){
+    Console.WriteLine("Max: " + number_1);
+    Console.WriteLine("Min: " + number_2);
+}
+if(number_1 < number_2){
+    Console.WriteLine("Max: " + number_2);
+    Console.WriteLine("Min: " + number_1);
+}
+if (number_1 == number_2){
+    Console.WriteLine("The numbers are equal");
+}
