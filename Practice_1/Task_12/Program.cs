6dff37f270a359f6e4009ec6541136c9f2b26a3d
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-08 23:14:46 +0400

    Completing Task_12

--- Practice_1/Task_12/Program.cs
+++ Practice_1/Task_12/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Удалить вторую цифру трёхзначного числа
+
+int DeleteSecondDigitInNimber(int number){
+    int thirdDigit = number % 10;
+    number = (number / 100) * 10 + thirdDigit;
+    return number;
+}
+
+Console.Write("Enter a three-digit number: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Result: " + DeleteSecondDigitInNimber(number));
