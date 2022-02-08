dda7ce00adb5c1a1e0d356114fea9203e02c7437
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-08 22:56:50 +0400

    Add some code into Task_13

--- Practice_1/Task_13/Program.cs
+++ Practice_1/Task_13/Program.cs
@@ -10,5 +10,6 @@ int number = int.Parse(Console.ReadLine());
 Console.Write("Inpunt divider: ");
 int divider = int.Parse(Console.ReadLine());
 
-if (numberDivider(number, divider) == 0) Console.WriteLine("Yes");
+if (number < divider) Console.WriteLine("Number < divider!");
+else if (numberDivider(number, divider) == 0) Console.WriteLine("Yes");
 else Console.WriteLine("No, remainder of the division: " + numberDivider(number, divider));
