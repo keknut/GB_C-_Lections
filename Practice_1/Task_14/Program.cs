1e5d4efa27aca5184f80a5973085dc647135f07a
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-08 23:44:16 +0400

    Completing Task_14

--- Practice_1/Task_14/Program.cs
+++ Practice_1/Task_14/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.Write("Input number: ");
+int number = int.Parse(Console.ReadLine());
+string s = number.ToString();
+if (s.Length > 2) Console.WriteLine("Third digit in number: " + s[2]);
+else Console.WriteLine("There is no third digit in the given number");
