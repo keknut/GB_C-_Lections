727433036edec31d25393d9d042bad4b0967a000
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-08 22:38:07 +0400

    Add some code into Task_11

--- Practice_1/Task_11/Program.cs
+++ Practice_1/Task_11/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа.
+
+int MaxNumber(int number){
+    int max = 0;
+    if(number % 10 > number / 10) max = number %10;
+    else max = number / 10;
+    return max;
+}
+
+int number = new Random().Next(10, 99);
+Console.WriteLine("Number: " + number);
+Console.WriteLine("Max: " + MaxNumber(number));
