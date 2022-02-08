b348bb313e21f6b608b2d1948fe895af9e022c5c
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-08 22:50:04 +0400

    Add some code into Task_13

--- Practice_1/Task_13/Program.cs
+++ Practice_1/Task_13/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int numberDivider(int number, int divider)
+{
+    return number % divider;
+}
+
+Console.Write("Input number: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Inpunt divider: ");
+int divider = int.Parse(Console.ReadLine());
+
+if (numberDivider(number, divider) == 0) Console.WriteLine("Yes");
+else Console.WriteLine("No, remainder of the division: " + numberDivider(number, divider));
