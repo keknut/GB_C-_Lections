54f3f69f99d0c627cc8cfadceb5476a716f5b112
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-08 23:03:30 +0400

    Add some code into Task_15

--- Practice_1/Task_15/Program.cs
+++ Practice_1/Task_15/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+Console.Write("Input number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0) Console.WriteLine("The number is divisible by 7 and 23");
+else Console.WriteLine("The number is not divisible by 7 and 23");
