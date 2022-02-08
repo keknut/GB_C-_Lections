ae316580152e9f87a18c48787f7a6baa87a92aa8
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-08 23:08:28 +0400

    Add completing Task_15

--- Practice_1/Task_10/Program.cs
+++ Practice_1/Task_10/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Write("Enter a three-digit number: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("The second digit in the number: " + number / 10 % 10);
