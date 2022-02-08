d2cec4138c98c25d0c3c87eae578b137b265cd79
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-08 23:52:54 +0400

    Completing Task_16

--- Practice_1/Task_16/Program.cs
+++ Practice_1/Task_16/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+bool weekend(int numberDayOfWeek){
+    if(numberDayOfWeek == 6 || numberDayOfWeek == 7) return true; //если день недели 6-ой или 7-ой (суббота или воскресенье), возвращает true
+    else return false; //иначе false
+}
+
+Console.Write("Input day of the week: ");
+int numberDayOfWeek = int.Parse(Console.ReadLine());
+
+if(weekend(numberDayOfWeek)) Console.WriteLine("Yes"); //обращение к методу weekend. Если true, то пишет Yes
+else Console.WriteLine("No"); //если false, то пишет No
