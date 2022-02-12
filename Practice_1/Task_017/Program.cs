8aec6c705b37e1296b6b195a1e285b945ad3859b
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-12 19:37:46 +0400

    Completing Task_017

--- Practice_1/Task_017/Program.cs
+++ Practice_1/Task_017/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+void squareNumber(int firstNumber, int secondNumber)
+{
+    bool flag = true;
+    if (firstNumber == secondNumber * secondNumber)
+    {
+        flag = false;
+        Console.WriteLine("The first number is the square of the second.");
+    }
+    if (firstNumber * firstNumber == secondNumber)
+    {
+        flag = false;
+        Console.WriteLine("The second number is the square of the first.");
+    }
+    if(flag) Console.WriteLine("Neither number is the square of the other");
+}
+
+Console.Write("Input first number: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Input second number: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+squareNumber(firstNumber, secondNumber);
