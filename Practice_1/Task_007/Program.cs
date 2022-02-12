853fc1a817a7126a00f15fd53bc43ac126876d80
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-12 17:51:21 +0400

    Completing task007

--- Practice_1/Task_007/Program.cs
+++ Practice_1/Task_007/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать числа от -N до N
+
+Console.Write("Input first number: ");
+int numberOne = int.Parse(Console.ReadLine());
+Console.Write("Input second number: ");
+int numberTwo = int.Parse(Console.ReadLine());
+
+if(numberTwo < numberOne){
+    int temporary = numberOne;
+    numberOne = numberTwo;
+    numberTwo = temporary;
+}
+
+while(numberOne <= numberTwo){
+    Console.Write($"{numberOne} ");
+    numberOne++;
+}
