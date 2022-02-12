134d6f2d70b65a64c47252a68ba57ee5f61d835a
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-12 21:09:13 +0400

    Completing Task_021

--- Practice_1/Task_021/Program.cs
+++ Practice_1/Task_021/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+void fiveDigitPalindrome(int number)
+{
+    if (number < 10000 || number > 99999) //если число не пятизначное, выводим сообщение
+    {
+        Console.WriteLine("Number is not five digit!");
+    }
+    else
+    {  //если первая цифра равна последней и если вторая цифра равна четвёртой
+        if (number / 10000 == number % 10 && (number / 1000) % 10 == (number / 10) % 10)
+        {
+            Console.WriteLine("The number is a palindrome.");
+        }
+        else
+        {
+            Console.WriteLine("The number is not a palindrome ");
+        }
+    }
+}
+Console.Write("Input a five-digit number: ");
+int number = int.Parse(Console.ReadLine());
+
+fiveDigitPalindrome(number);
