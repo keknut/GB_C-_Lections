624032073638b75f9ed3d48f4189c26c5a5fe64d
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-12 20:49:27 +0400

    Completing Task_019

--- Practice_1/Task_019/Program.cs
+++ Practice_1/Task_019/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+void wherePoint(int x, int y)
+{
+    if (x > 0 && y > 0) Console.WriteLine("Point in the first quarter (I)");
+    if (x < 0 && y > 0) Console.WriteLine("Point in the second quarter (II)");
+    if (x < 0 && y < 0) Console.WriteLine("Point in the third quarter (III)");
+    if (x > 0 && y < 0) Console.WriteLine("Point in the fourth quarter (IV)");
+    if (x==0 || y == 0) Console.WriteLine("X or Y equal zero. Quarter is impossible to determine.");
+}
+
+Console.Write("Input X: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Input Y: ");
+int y = int.Parse(Console.ReadLine());
+
+wherePoint(x, y);
