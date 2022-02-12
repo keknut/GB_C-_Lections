8edda931d432d47854b9e7b4c6513d9060f32afb
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-12 22:59:12 +0400

    Completing Task_024

--- Practice_1/Task_024/Program.cs
+++ Practice_1/Task_024/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти кубы чисел от 1 до N
+
+void cubeTable(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine($"{i}^3 = {i * i * i}");
+    }
+}
+
+Console.Write("Input number: ");
+int n = int.Parse(Console.ReadLine());
+
+cubeTable(n);
