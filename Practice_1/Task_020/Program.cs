271c560e8318bd78ea553f7a23b18f73bb4f3658
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-12 22:30:16 +0400

    Completing Task_020

--- Practice_1/Task_020/Program.cs
+++ Practice_1/Task_020/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+void coordinateRanges(byte quarterNumber)
+{
+    if (quarterNumber == 1)
+    {
+        Console.WriteLine("X>0, Y>0");
+    }
+    if (quarterNumber == 2)
+    {
+        Console.WriteLine("X<0, Y>0");
+    }
+    if (quarterNumber == 3)
+    {
+        Console.WriteLine("X<0, Y<0");
+    }
+    if (quarterNumber == 4)
+    {
+        Console.WriteLine("X>0, Y<0");
+    }
+    if (quarterNumber < 1 || quarterNumber > 4)
+    {
+        Console.WriteLine("The quarter number must be between 1 and 4!");
+    }
+}
+
+Console.Write("Enter the quarter number: ");
+byte quarterNumber = byte.Parse(Console.ReadLine());
+
+coordinateRanges(quarterNumber);
