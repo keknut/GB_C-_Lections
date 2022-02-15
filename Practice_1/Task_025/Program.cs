74df95c001f3967a279a4aa4b7149f1b8562a5c0
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-15 21:56:27 +0400

    Completing Task_025

--- Practice_1/Task_025/Program.cs
+++ Practice_1/Task_025/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти сумму чисел от 1 до А
+
+int sum(int number) //
+{
+    int sum = 0;
+    //sum = number * (number +1) / 2; //можно через формулу. Если вычислять через формулу при большом number, то времени будет затрачено меньше.
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+Console.Write("Input number: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Summ 1 to {number} = {sum(number)}");
