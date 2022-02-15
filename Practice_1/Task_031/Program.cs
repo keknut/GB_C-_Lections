bb55304f8d53063638171f61685545ab3872d779
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-15 23:51:04 +0400

    Modified Task_031

--- Practice_1/Task_031/Program.cs
+++ Practice_1/Task_031/Program.cs
@@ -1,8 +1,17 @@
 ﻿// Задать массив из 8 элементов и вывести их на экран
 
+void outputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
 int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
-for (int i = 0; i < array.Length; i++)
+outputArray(array);
+/*for (int i = 0; i < array.Length; i++)
 {
     Console.Write($"{array[i]} ");
-}
+}*/
