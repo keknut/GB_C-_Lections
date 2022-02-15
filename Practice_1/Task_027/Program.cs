1ab7021064b7ca76822b5f07363f9ea574711037
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-15 22:55:13 +0400

    Completing Task_027

--- Practice_1/Task_027/Program.cs
+++ Practice_1/Task_027/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Определить количество цифр в числе
+
+int NumberOfDigitsInNumber(int number)
+{
+    int i = 0;
+    while (number >= 1)
+    {
+        number = number / 10;
+        i++;
+    }
+    return i;
+}
+
+Console.Write("Input number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Number of digits in a {number}: {NumberOfDigitsInNumber(number)}");
