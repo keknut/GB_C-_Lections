8a19e0235cc3710f5891f1e965b1a54a49091ee7
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-15 23:46:49 +0400

    Completing Task_028

--- Practice_1/Task_028/Program.cs
+++ Practice_1/Task_028/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Подсчитать сумму цифр в числе
+
+int sumDigitsNumber(int number)
+{
+    int result = 0;
+    while (number >= 1)
+    {
+        result = result + number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+
+Console.Write("Input number: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Sum of the digits in a {number}: {sumDigitsNumber(number)}");
