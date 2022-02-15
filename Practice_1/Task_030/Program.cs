8e4e260615334ab29a110eda350aef0c1869c756
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-16 00:02:50 +0400

    Completing Task_030

--- Practice_1/Task_030/Program.cs
+++ Practice_1/Task_030/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+/*Возможно имеллось в виду вывести кубы чисел от 1 до N заканчивающихся на четные цифры.
+*/
+
+void cubesEvenNumbers(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.WriteLine($"{i}^3: {i*i*i}");
+        }
+    }
+}
+
+Console.Write("Input N: ");
+int n = int.Parse(Console.ReadLine());
+
+cubesEvenNumbers(n);
