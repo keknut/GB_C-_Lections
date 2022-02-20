35a5b0e02594e3dd0f1638a318dcc0846c954cb1
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-20 14:14:41 +0400

    Completing Task_038

--- Practice_1/Task_038/Program.cs
+++ Practice_1/Task_038/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+void FillArray(int[] array) //метод заполняет массив случайными числами [0; 10]
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+}
+
+void PrintArray(int[] array) //метод выводит массив в консоль
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int SummOddIndex(int[] array){
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            result = result + array[i];
+        }
+    }
+    return result;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine($"Sum of array elements with odd index: {SummOddIndex(array)}");
