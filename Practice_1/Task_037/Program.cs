87644e20b7675ac0208f011ace0d87e1e605d3e7
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-20 13:28:50 +0400

    Completing Task_037

--- Practice_1/Task_037/Program.cs
+++ Practice_1/Task_037/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+void FillArray(int[] array) //метод заполняет массив случайными числами [0; 199]
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 200);
+    }
+}
+
+void PrintArray(int[] array) //метод выводит массив в консоль
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int AmountElements(int[] array)//возвращает количество элементов в массиве на отрезке [10; 99]
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+int[] array = new int[123];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"Amount of elements [10; 99] in array: {AmountElements(array)}");
