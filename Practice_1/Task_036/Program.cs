840a09b9a8095d60c32f5d0c162c16f17c64cea4
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-20 14:10:39 +0400

    Completing Task_036

--- Practice_1/Task_036/Program.cs
+++ Practice_1/Task_036/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами.
+//Показать количество нечетных\четных чисел
+
+void FillArray(int[] array) //метод заполняет массив случайными числами [100; 1000]
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] array) //метод выводит массив в консоль
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int EvenNumbers(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+int OddNumbers(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine($"Number of even numbers in the array: {EvenNumbers(array)}");
+Console.WriteLine($"Number of odd numbers in the array: {OddNumbers(array)}");
