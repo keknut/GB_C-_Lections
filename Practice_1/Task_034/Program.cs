87432f817a28f263aaa4e6c0afd0412b9da758c4
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-20 14:03:01 +0400

    Completing Task_034

--- Practice_1/Task_034/Program.cs
+++ Practice_1/Task_034/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу замену элементов массива на противоположные
+
+void FillArray(int[] array) //метод заполняет массив случайными числами [-9; 9]
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+}
+
+void PrintArray(int[] array) //метод выводит массив в консоль
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void ReverseArray(int[] array){
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * (-1);
+    }
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+ReverseArray(array);
+PrintArray(array);
