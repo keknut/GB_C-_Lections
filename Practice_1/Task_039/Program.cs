2611ee3a60bb6bb22e2c1e60141d16e1de726099
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-20 13:59:28 +0400

    Completing Task_039

--- Practice_1/Task_039/Program.cs
+++ Practice_1/Task_039/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти произведение пар чисел в одномерном массиве.
+//Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+void FillArray(int[] array) //метод заполняет массив случайными числами [0; 9]
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+}
+
+void PrintArray(int[] array) //метод выводит массив в консоль
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SumPairs(int[] array)
+{
+    for (int i = 0; i < (array.Length + 1) / 2; i++)
+    {
+        Console.WriteLine($"Product of pairs  {i} & {array.Length - i}: {array[i] * array[array.Length - 1 - i]}");
+    }
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+SumPairs(array);
