0e6112992f2c92cca2c8315e82d5645b17ff5013
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-23 15:33:23 +0400

    Completing Task_041

--- Practice_1/Task_041/Program.cs
+++ Practice_1/Task_041/Program.cs
@@ -1,7 +1,13 @@
 ﻿// Выяснить являются ли три числа сторонами треугольника
 
 void TriangleInequality(int FirstNumber, int SecondNumber, int ThirdNumber){
-    if(FirstNumber + SecondNumber > ThirdNumber && FirstNumber + ThirdNumber > SecondNumber )
+    if(FirstNumber + SecondNumber > ThirdNumber 
+        && FirstNumber + ThirdNumber > SecondNumber
+        && SecondNumber + ThirdNumber > FirstNumber){
+            Console.WriteLine($"Numbers {FirstNumber}, {SecondNumber} and {ThirdNumber} are the sides of the triangle.");
+        } else{
+            Console.WriteLine($"Numbers {FirstNumber}, {SecondNumber} and {ThirdNumber} are not sides of a triangle.");
+        }
 } 
 
 Console.Write($"Input first number: ");
@@ -10,3 +16,5 @@ Console.Write($"Input second number: ");
 int SecondNumber = int.Parse(Console.ReadLine());
 Console.Write($"Input third number: ");
 int ThirdNumber = int.Parse(Console.ReadLine());
+
+TriangleInequality(FirstNumber, SecondNumber, ThirdNumber);
