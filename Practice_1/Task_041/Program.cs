f31d77174e8a5e9d4c4bc35e4ef21fdb72abf7e9
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-23 15:00:56 +0400

    Modified Task_041

--- Practice_1/Task_041/Program.cs
+++ Practice_1/Task_041/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Выяснить являются ли три числа сторонами треугольника
+
+void TriangleInequality(int FirstNumber, int SecondNumber, int ThirdNumber){
+    if(FirstNumber + SecondNumber > ThirdNumber && FirstNumber + ThirdNumber > SecondNumber )
+} 
+
+Console.Write($"Input first number: ");
+int FirstNumber = int.Parse(Console.ReadLine());
+Console.Write($"Input second number: ");
+int SecondNumber = int.Parse(Console.ReadLine());
+Console.Write($"Input third number: ");
+int ThirdNumber = int.Parse(Console.ReadLine());
