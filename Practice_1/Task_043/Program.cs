0358de46a18b957a4ddfd21faa83f30f75566071
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-23 16:47:35 +0400

    Completing Task_043

--- Practice_1/Task_043/Program.cs
+++ Practice_1/Task_043/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу преобразования десятичного числа в двоичное
+
+string DecimalToBinary(int number)
+{
+    string result = "";
+    while (number > 0) { 
+        if(number % 2 == 0){
+            result = "0" + result; 
+        } else{
+            result = "1" + result;
+        }
+        number = number / 2;
+    }
+    return result;
+}
+
+Console.Write("Input decimal number: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Decimal to binary: {DecimalToBinary(number)}");
+//Console.WriteLine(number % 2);
