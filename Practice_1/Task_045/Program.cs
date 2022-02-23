9d9360d9e51996934a7c78719697fe4390d2320c
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-24 01:39:57 +0400

    Completing Task_045

--- Practice_1/Task_045/Program.cs
+++ Practice_1/Task_045/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать числа Фибоначчи
+
+void Fibonacci(int limit){
+    int numberZero = 0;
+    int numberOne = 1;
+    int temp = 0;
+    Console.Write($"{numberZero} {numberOne} ");
+    for(int i = 0; i <= limit; i++){
+        temp = numberZero + numberOne;
+        Console.Write($"{temp} ");
+        numberZero = numberOne;
+        numberOne = temp;
+    }
+}
+
+Console.Write("Input upper limit: ");
+int limit = int.Parse(Console.ReadLine());
+
+Fibonacci(limit);
