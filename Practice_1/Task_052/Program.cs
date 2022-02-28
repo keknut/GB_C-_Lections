4d25e77dcd11c3d1b2b8be7e51622ea5c0d67e07
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-28 20:07:32 +0400

    Completing Task_052

--- Practice_1/Task_052/Program.cs
+++ Practice_1/Task_052/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+
+void FillArray(int[,] array) //метод заполняет массив случайными числами [0; 10]
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array) //метод выводит массив в консоль
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] = array[i, j] * array[i, j];
+                Console.ForegroundColor = ConsoleColor.Green; // Если условие выполнилось, меняем цвет текста в консоли.
+            }
+            Console.Write($"{array[i, j]} "); // Печатаем массив
+            Console.ResetColor(); // Сбрасываем цвет текста в консоли
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Enter the dimension of the array.");
+Console.Write("Input m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Input n: ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+ChangeArray(array);
