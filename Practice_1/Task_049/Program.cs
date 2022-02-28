e0375a00fc3ae401d4c52abf0426a14abfa7ecd7
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-28 21:10:24 +0400

    Completing Task_054

--- Practice_1/Task_049/Program.cs
+++ Practice_1/Task_049/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+void FillArray(double[,] array) //метод заполняет массив случайными числами [0; 10]
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().NextDouble(0, 1);
+        }
+    }
+}
+
+void PrintArray(double[,] array) //метод выводит массив в консоль
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Enter the dimension of the array.");
+Console.Write("Input m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Input n: ");
+int n = int.Parse(Console.ReadLine());
+double[,] array = new double[m, n];
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
--- Practice_1/Task_054/Program.cs
+++ Practice_1/Task_054/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// В матрице чисел найти сумму элементов главной диагонали
+
+void FillArray(int[,] array) //метод заполняет массив случайными числами [0; 10]
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array) //метод выводит массив в консоль
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i == j){
+                Console.ForegroundColor = ConsoleColor.Green; // Подкрашиваем главную диагональ для удобства, изменяя цвет текста.
+            }
+            Console.Write($"{array[i, j]} ");
+            Console.ResetColor(); // Сбрасываем цвет текста в консоли
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumElementsMainDiagonal(int[,] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                result = result + array[i, j];
+            }
+        }
+    }
+    return result;
+}
+
+Console.WriteLine("Enter the dimension of the array.");
+Console.Write("Input m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Input n: ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine($"The sum of the elements of the main diagonal: {SumElementsMainDiagonal(array)}");
