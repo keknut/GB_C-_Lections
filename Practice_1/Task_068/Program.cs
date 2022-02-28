60f49cbafa406ff31a7a93769938d6d7ba29708f
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-28 11:39:08 +0400

    Completing Task_068

---
+++ Practice_1/Task_068/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void PrintNumber(int numberM, int numberN)
+{
+    if (numberN < numberM) return;
+    PrintNumber(numberM, numberN - 1);
+    Console.Write($"{numberN} ");
+}
+
+Console.Write("Inpunt number M: ");
+int numberM = int.Parse(Console.ReadLine());
+Console.Write("Inpunt number N: ");
+int numberN = int.Parse(Console.ReadLine());
+PrintNumber(numberM, numberN);
