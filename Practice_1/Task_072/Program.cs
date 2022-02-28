9445f6411ff74623c812e1b819be40846d7b758b
nikita.mishchenko <nekitoso2@yandex.ru>
2022-02-28 11:59:55 +0400

    Completing Task_072

--- Practice_1/Task_072/Program.cs
+++ Practice_1/Task_072/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу возведения числа А в целую стень B
+
+int DegreeOfNumber(int numberA, int numberB){
+    if(numberB < 1) return 1;
+    return numberA * (DegreeOfNumber(numberA, numberB - 1));
+}
+
+Console.Write("Inpunt number A: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Inpunt number B: ");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Degree of {numberA} in {numberB}: {DegreeOfNumber(numberA, numberB)}");
