792501e79b94e3411e14329ae6d0fea3304f951f
nikita.mishchenko <nekitoso2@yandex.ru>
2022-03-17 18:18:58 +0300

    Completing Task_057

--- Practice/Task_057/Program.cs
+++ Practice/Task_057/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, упорядочивания по убыванию
+// элементы каждой строки двумерной массива.
+
+void FillArray(int[,] array) //метод заполняет массив случайными числами [0; 10]
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array) //метод выводит массив в консоль.
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortingLines(int[,] array)
+{
+    // Перебираем каждый элемент массива (кроме последнего, он уже будет отсортирован к тому времени, когда мы до него доберемся)
+    int tmp = 0;
+    int maxIndex = 0;
+    int startIndex = 0;
+    int currentIndex = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (startIndex = 0; startIndex < array.GetLength(1) - 1; startIndex++)
+        {
+            // В переменной maxIndex хранится индекс наибольшего значения, которое мы нашли в этой итерации.
+            // Начинаем с того, что наибольший элемент в этой итерации - это первый элемент (индекс 0)
+            maxIndex = startIndex;
+
+            // Затем ищем элемент побольше в остальной части массива
+            for (currentIndex = startIndex + 1; currentIndex < array.GetLength(1); currentIndex++)
+            {
+                // Если мы нашли элемент, который больше нашего наибольшего элемента,
+                if (array[i, currentIndex] > array[i, maxIndex])
+                    // то запоминаем его
+                    maxIndex = currentIndex;
+            }
+            // maxIndex теперь наибольший элемент.
+            // Меняем местами наше начальное наибольшее число с тем, которое мы обнаружили
+            tmp = array[i, startIndex];
+            array[i, startIndex] = array[i, maxIndex];
+            array[i, maxIndex] = tmp;
+        }
+    }
+}
+
+Console.WriteLine("Enter the dimension of the array.");
+Console.Write("Input m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Input n: ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+
+FillArray(array);
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+SortingLines(array);
+Console.WriteLine();
+PrintArray(array);
--- Practice/Tests/Program.cs
+++ Practice/Tests/Program.cs
@@ -1,36 +1,58 @@
-﻿void FillArray(int[,] array) //метод заполняет массив случайными числами [0; 10]
+﻿void FillArray(int[] array) //метод заполняет массив случайными числами [0; 10]
 {
     int k = 0;
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[j, i] = k;
+            array[i] = k;
             k++;
-        }
     }
 }
 
-void PrintArray(int[,] array) //метод выводит массив в консоль.
+void PrintArray(int[] array) //метод выводит массив в консоль.
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]} ");
-        }
-        Console.WriteLine();
+            Console.Write($"{array[i]} ");
     }
+}
+
+void SortingLines(int[] array)
+{
+    // Перебираем каждый элемент массива (кроме последнего, он уже будет отсортирован к тому времени, когда мы до него доберемся)
+    int tmp = 0;
+    int maxIndex = 0;
+    int startIndex = 0;
+    int currentIndex = 0;
+        for (startIndex = 0; startIndex < array.GetLength(0) - 1; startIndex++)
+        {
+            // В переменной maxIndex хранится индекс наименьшего значения, которое мы нашли в этой итерации.
+            // Начинаем с того, что наименьший элемент в этой итерации - это первый элемент (индекс 0)
+            maxIndex = startIndex;
+
+            // Затем ищем элемент поменьше в остальной части массива
+            for (currentIndex = startIndex + 1; currentIndex < array.GetLength(0); currentIndex++)
+            {
+                // Если мы нашли элемент, который меньше нашего наименьшего элемента,
+                if (array[currentIndex] > array[maxIndex])
+                    // то запоминаем его
+                    maxIndex = currentIndex;
+            }
+            // maxIndex теперь наименьший элемент.
+            // Меняем местами наше начальное наименьшее число с тем, которое мы обнаружили
+            tmp = array[startIndex];
+            array[startIndex] = array[maxIndex];
+            array[maxIndex] = tmp;
+        }
 }
 
 Console.WriteLine("Enter the dimension of the array.");
 Console.Write("Input m: ");
 int m = int.Parse(Console.ReadLine());
-Console.Write("Input n: ");
-int n = int.Parse(Console.ReadLine());
-int[,] array = new int[m, n];
+int[] array = new int[m];
 
 FillArray(array);
 Console.WriteLine();
 PrintArray(array);
 Console.WriteLine();
+SortingLines(array);
+PrintArray(array);
