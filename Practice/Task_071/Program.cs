6a7f8218612b9bda561067e5ecd1174f6e48c8d4
nikita.mishchenko <nekitoso2@yandex.ru>
2022-03-18 21:20:44 +0300

    Completing Task_071

--- Practice/Task_071/Program.cs
+++ Practice/Task_071/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу вычисления функции Аккермана
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 3, n = 3 -> A(m,n) = 29
+
+int Akkerman(int numberM, int numberN){
+    if(numberM == 0){
+        return numberN + 1;
+    }else{
+        if((numberM != 0) && (numberN == 0)){
+            return Akkerman(numberM - 1, 1);
+        }else{
+            return Akkerman(numberM - 1, Akkerman(numberM, numberN-1));
+        }
+    }
+}
+
+Console.Write("Inpunt M: ");
+int numberM = int.Parse(Console.ReadLine());
+Console.Write("Inpunt N: ");
+int numberN = int.Parse(Console.ReadLine());
+Console.WriteLine($"Akkerman({numberM},{numberN}): {Akkerman(numberM, numberN)}");
