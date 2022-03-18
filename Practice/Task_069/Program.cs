10b20cb0a1ca3ad2a754175d5c25206934c4ceeb
nikita.mishchenko <nekitoso2@yandex.ru>
2022-03-18 20:58:31 +0300

    Completing Task_069

--- Practice/Task_069/Program.cs
+++ Practice/Task_069/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу,
+// которая найдёт сумму элементов в промежутке от M до N.
+// M = 1; N = 4 -> 10
+// M = 4; N = 8. -> 30
+
+int SumNumbers(int numberM, int numberN)
+{
+    if (numberN < numberM) return 0;
+    return SumNumbers(numberM, numberN - 1) + numberN;
+}
+
+Console.Write("Inpunt M: ");
+int numberM = int.Parse(Console.ReadLine());
+Console.Write("Inpunt N: ");
+int numberN = int.Parse(Console.ReadLine());
+Console.WriteLine($"Summ: {SumNumbers(numberM, numberN)}");
