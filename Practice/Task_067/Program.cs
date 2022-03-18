a05ab030f1ddcd63de739e04d270e4d77012b118
nikita.mishchenko <nekitoso2@yandex.ru>
2022-03-18 20:39:20 +0300

    Completing Task_067

--- Practice/Task_067/Program.cs
+++ Practice/Task_067/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать натуральные числа от N до 1, N задано.
+
+void PrintNumber(int number)
+{
+    if (number < 1) return;
+    Console.Write($"{number} ");
+    PrintNumber(number - 1);
+}
+
+Console.Write("Inpunt N: ");
+int number = int.Parse(Console.ReadLine());
+PrintNumber(number);
