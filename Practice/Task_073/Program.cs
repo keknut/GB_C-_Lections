d6e82d898c538b271886742df17f63c1c2f6e886
nikita.mishchenko <nekitoso2@yandex.ru>
2022-03-19 16:20:35 +0300

    Completing Task_073

--- Practice/Task_073/Program.cs
+++ Practice/Task_073/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+// Первые два элемента последовательности задаются пользователем
+
+int Summ(int firstNumber, int secondNumber, int numberN){
+    if(numberN == 1) return firstNumber;
+    if(numberN == 2) return secondNumber;
+    return Summ(firstNumber, secondNumber, numberN - 1) + Summ(firstNumber, secondNumber, numberN - 2);
+}
+
+Console.Write("Input first number: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Input second number: ");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.Write("Enter number of numbers : ");
+int numberN = int.Parse(Console.ReadLine());
+
+
+for (int i = 1; i <= numberN; i++)
+{
+    Console.Write($"{Summ(firstNumber, secondNumber, i)} ");   
+}
